48959736fd9b4b0893bcec3f67f35a42ff18704c
smige1 <egimsikana@gmail.com>
2023-07-13 01:27:54 +0100

    dashboard

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -30,11 +30,14 @@ export const StoreModal = () => {
       name: "",
     },
   });
+  
+  
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       setLoading(true);
       const response = await axios.post('/api/stores', values);
+     
       window.location.assign(`/${response.data.id}`);
     } catch (error) {
       toast.error('Something went wrong');

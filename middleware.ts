12f3d464bbb8b5154f401065d8a14c1ad8f9489d
smige1 <egimsikana@gmail.com>
2023-07-25 17:35:00 +0100

    make changes

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,8 @@ export default authMiddleware({
     "/api/a4cdf241-45b8-4309-9126-cec6707574fc/sizes",
     "/api/a4cdf241-45b8-4309-9126-cec6707574fc/colors",
     "/api/a4cdf241-45b8-4309-9126-cec6707574fc/products",
-    "/api/a4cdf241-45b8-4309-9126-cec6707574fc/billboards/eec34c19-8361-420c-b92f-9e82d0c77f15"
+    "/api/a4cdf241-45b8-4309-9126-cec6707574fc/billboards/eec34c19-8361-420c-b92f-9e82d0c77f15",
+    "/api/a4cdf241-45b8-4309-9126-cec6707574fc/billboards/5c312999-360d-4a8a-9c7d-32bec45ac764"
   ]
     
 });
